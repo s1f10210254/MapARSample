294a909e03b7be9108a8c8b75bb24c3366b74bdb
s1f10210254 <s1f102102548@iniad.org>
2024-01-18 23:55:14 +0900

    List

--- MapARSample/Views/SecondView.swift
+++ MapARSample/Views/SecondView.swift
@@ -6,6 +6,17 @@ struct SecondView: View {
     var body: some View {
         // SecondView の UI と、viewModel を利用した処理を追加する
       Text("Second View").padding()
+      List {
+        Text("1").onTapGesture{
+          print("1")
+        }
+        Text("2").onTapGesture{
+          print("2")
+        }
+        Text("3").onTapGesture{
+          print("3")
+        }
+      }
 
     }
 }
