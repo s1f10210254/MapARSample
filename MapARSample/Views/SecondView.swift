7aaf3adc9769767f64bc1f7310d6bbf5bafc8846
s1f10210254 <s1f102102548@iniad.org>
2024-01-19 23:56:19 +0900

    back

--- MapARSample/Views/SecondView.swift
+++ MapARSample/Views/SecondView.swift
@@ -1,29 +1,18 @@
 import SwiftUI
 
 struct SecondView: View {
-    @ObservedObject var viewModel: SecondViewModel
+  @ObservedObject var viewModel: SecondViewModel
+  @State var menuOpen: Bool = false
+  var body: some View {
+    // SecondView の UI と、viewModel を利用した処理を追加する
+    Text("Second View").font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
 
-    var body: some View {
-        // SecondView の UI と、viewModel を利用した処理を追加する
-      Text("Second View").padding()
-      List {
-        Text("1").onTapGesture{
-          print("1")
-        }
-        Text("2").onTapGesture{
-          print("2")
-        }
-        Text("3").onTapGesture{
-          print("3")
-        }
-      }
-
-    }
+  }
 }
 
 struct SecondView_Previews: PreviewProvider {
-    static var previews: some View {
-        SecondView(viewModel: SecondViewModel())
-    }
+  static var previews: some View {
+    SecondView(viewModel: SecondViewModel())
+  }
 }
 
